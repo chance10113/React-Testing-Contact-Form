ef1c47b18e9ecfc151acb43bff8fa62cc782cb8b
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-02-11 17:08:09 -0600

    Added useEvents to all the fields, and a query for the button. Looking at the form, there isn't a button, as far as i can understand it, there is just an input type='submit', and.... That works!

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -12,9 +12,25 @@ test("Form can be filled out, submit adds new info", () => {
   render(<ContactForm />);
   // Input Queries
   // const firstNameInput = screen.getByLabelText(/first name/i)
-  const firstNameInput = screen.getByPlaceholderText(/edd/i)
+  const firstNameInput = screen.getByPlaceholderText(/edd/i);
   const lastNameInput = screen.getByLabelText(/last name/i);
   // const emailInput = screen.getByLabelText(/email/i)
-  const emailInput = screen.getByPlaceholderText(/bluebill1049@hotmail.com/i)
+  const emailInput = screen.getByPlaceholderText(/bluebill1049@hotmail.com/i);
   const messageInput = screen.getByLabelText(/message/i);
+
+  // Can type into inputs
+  userEvent.type(firstNameInput, "Cha");
+  userEvent.type(lastNameInput, "Chez");
+  userEvent.type(emailInput, "Chaz@Chez.com");
+  userEvent.type(messageInput, "ChezzyChaz!!");
+
+  // Expects
+  expect(firstNameInput).toHaveValue("Cha");
+  expect(lastNameInput).toHaveValue("Chez");
+  expect(emailInput).toHaveValue("Chaz@Chez.com");
+  expect(messageInput).toHaveValue("ChezzyChaz!!");
+
+  // Button Query
+  const button = screen.getByRole("button") // Apparently this is a BUTTON!!!!
+  
 });
